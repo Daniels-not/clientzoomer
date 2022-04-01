13a0eb2c189531d62cbf7c2aabb1647bb13fe331
Daniels-not <ramydcampusano@hotmail.com>
2022-03-31 21:06:08 -0400

    error fixed in the socked

--- src/SocketConfigProvider.js
+++ src/SocketConfigProvider.js
@@ -4,7 +4,7 @@ import Peer from 'simple-peer';
 
 const SocketContext = createContext();
 
-const socket = io('https://clientzoomer.herokuapp.com/');
+const socket = io('https://warm-wildwood-81069.herokuapp.com');
 
 const ContextProvider = ({ children }) => {
   const [callAccepted, setCallAccepted] = useState(false);
--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -59,37 +59,37 @@ const Options = ({ children }) => {
 
   return (
     <div>
-      <Container className={classes.container}>
-        <Paper elevation={10} className={classes.paper}>
-          <form className={classes.root} noValidate autoComplete="off">
-            <Grid container className={classes.gridContainer}>
-              <Grid item xs={12} md={6} className={classes.padding}>
-                <Typography gutterBottom variant="h6">Account Info</Typography>
-                <TextField label="Name" value={name} onChange={(e) => setName(e.target.value)} fullWidth />
-                <CopyToClipboard text={me} className={classes.margin}>
+    <Container className={classes.container}>
+      <Paper elevation={10} className={classes.paper}>
+        <form className={classes.root} noValidate autoComplete="off">
+          <Grid container className={classes.gridContainer}>
+            <Grid item xs={12} md={6} className={classes.padding}>
+              <Typography gutterBottom variant="h6">Account Info</Typography>
+              <TextField label="Name" value={name} onChange={(e) => setName(e.target.value)} fullWidth />
+              <CopyToClipboard text={me} className={classes.margin}>
                 <Button variant="contained" color="primary" fullWidth startIcon={<Assignment fontSize="large" />}>
                   Copy Your ID
                 </Button>
               </CopyToClipboard>
-              </Grid>
-              <Grid item xs={12} md={6} className={classes.padding}>
-                <Typography gutterBottom variant="h6">Make a call</Typography>
-                <TextField label="ID to call" value={idCall} onChange={(e) => setIdCall(e.target.value)} fullWidth />
-                {callAccepted && !callEnded ? (
-                  <Button variant="contained" color="secondary" startIcon={<PhoneDisabled fontSize="large" />} fullWidth onClick={leaveCall} className={classes.margin}>
-                    Hang Up
-                  </Button>
-                ) : (
-                  <Button variant="contained" color="primary" startIcon={<Phone fontSize="large" />} fullWidth onClick={() => callUser(idCall)} className={classes.margin}>
-                    Call
-                  </Button>
-                )}
-              </Grid>
             </Grid>
-          </form>
-          {children}
-        </Paper>
-      </Container>
+            <Grid item xs={12} md={6} className={classes.padding}>
+              <Typography gutterBottom variant="h6">Make a call</Typography>
+              <TextField label="ID to call" value={idCall} onChange={(e) => setIdCall(e.target.value)} fullWidth />
+              {callAccepted && !callEnded ? (
+                <Button variant="contained" color="secondary" startIcon={<PhoneDisabled fontSize="large" />} fullWidth onClick={leaveCall} className={classes.margin}>
+                  Hang Up
+                </Button>
+              ) : (
+                <Button variant="contained" color="primary" startIcon={<Phone fontSize="large" />} fullWidth onClick={() => callUser(idCall)} className={classes.margin}>
+                  Call
+                </Button>
+              )}
+            </Grid>
+          </Grid>
+        </form>
+        {children}
+      </Paper>
+    </Container>
     </div>
   )
 }
